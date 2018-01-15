25d6491fdbd89ea0f5ab60d5101afec1b75b5e58
samolus <assoulinesamuel@gmail.com>
2018-01-15 15:35:06 +0100

    2nd test

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -3,8 +3,6 @@ package fr.esiea;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GildedRoseTest {
     @Test
     public void non_specific_object_quality_and_price_decrease_by_one() {
@@ -21,6 +19,25 @@ public class GildedRoseTest {
                 .isEqualTo(9);
         solftly.assertThat(item.sellIn)
                 .as("Apple Price")
+                .isEqualTo(1);
+        solftly.assertAll();
+    }
+
+    @Test
+    public void Aged_Brie_Equals_and_Quality_increase() {
+        Item item = new Item("Aged Brie", 2, 10);
+        Item[] items = new Item[] {item};
+
+        GildedRose tavern = new GildedRose(items);
+
+        tavern.updateQuality();
+
+        SoftAssertions solftly = new SoftAssertions();
+        solftly.assertThat(item.quality)
+                .as("Aged Brie quality")
+                .isEqualTo(11);
+        solftly.assertThat(item.sellIn)
+                .as("Aged Brie Price")
                 .isEqualTo(1);
         solftly.assertAll();
     }
