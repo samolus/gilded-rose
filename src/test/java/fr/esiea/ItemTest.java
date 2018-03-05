f11428ec5d371188c425a93bf6965dbce4efdfc8
jbuniak <jbuniak@JBUNIAK-FR.fr.oracle.com>
2018-03-05 11:33:27 +0100

    Refactoring With Method updateDefaultItem

--- src/test/java/fr/esiea/ItemTest.java
+++ src/test/java/fr/esiea/ItemTest.java
@@ -7,24 +7,6 @@ import static org.junit.Assert.*;
 
 public class ItemTest {
 
-    /*@Test
-    public void non_specific_object_quality_and_price_decrease_by_one() {
-        Item item = new Item("apple", 2, 10);
-        Item[] items = new Item[] {item};
-
-        GildedRose tavern = new GildedRose(items);
-
-        tavern.updateQuality();
-
-        SoftAssertions solftly = new SoftAssertions();
-        solftly.assertThat(item.quality)
-                .as("Apple quality")
-                .isEqualTo(9);
-        solftly.assertThat(item.sellIn)
-                .as("Apple Price")
-                .isEqualTo(1);
-        solftly.assertAll();
-    }*/
 
     @Test
     public void testToString()
